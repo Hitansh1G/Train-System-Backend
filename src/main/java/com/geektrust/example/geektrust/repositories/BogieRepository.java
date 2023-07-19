0b894bf6cf430a3cd2bdf124ef269dea044f4e4c
Hitansh1G <hitanshgupta007@gmail.com>
2023-07-19 16:25:28 +0530

    Load Data Repository

--- src/main/java/com/geektrust/example/geektrust/repositories/BogieRepository.java
+++ src/main/java/com/geektrust/example/geektrust/repositories/BogieRepository.java
@@ -7,45 +7,36 @@ import java.util.Optional;
 import com.geektrust.example.geektrust.entities.Bogie;
 
 public class BogieRepository implements IBogieRepository {
-    private HashMap<String, Bogie> boggyMapping;
+    private HashMap<String, Bogie> bogieListing;
     private Integer autoIncrement = 0;
 
     public BogieRepository() {
-        this.boggyMapping = new HashMap<String, Bogie>();
+        this.bogieListing = new HashMap<String, Bogie>();
     }
 
-    public BogieRepository(HashMap<String, Bogie> boggyMapping){
-        this.boggyMapping = boggyMapping;
+    public BogieRepository(HashMap<String, Bogie> bogieListing){
+        this.bogieListing = bogieListing;
     }
 
-//    @Override
-//    public Bogie saveBoggy(Bogie bogie) {
-//        if(bogie.getId() == null){
-//            autoIncrement++;
-//            Bogie newBogie = new Bogie(Integer.toString(autoIncrement), bogie.getdestinationStation());
-//            boggyMapping.put(newBogie.getId(), newBogie);
-//            return newBogie;
-//        }
-//        boggyMapping.put(bogie.getId(), bogie);
-//        return bogie;
-//    }
-
-    @Override
-    public HashMap<String, Bogie> getBoggyMapping() {
-        return boggyMapping;
-    }
 
     @Override
     public Bogie save(Bogie bogie) {
         if(bogie.getId() == null){
             autoIncrement++;
             Bogie newBogie = new Bogie(Integer.toString(autoIncrement), bogie.getdestinationStation());
-            boggyMapping.put(newBogie.getId(), newBogie);
+            bogieListing.put(newBogie.getId(), newBogie);
             return newBogie;
         }
-        boggyMapping.put(bogie.getId(), bogie);
+        bogieListing.put(bogie.getId(), bogie);
         return bogie;
     }
+
+
+    @Override
+    public HashMap<String, Bogie> getBogieListing() {
+        return bogieListing;
+    }
+
 
     @Override
     public List<Bogie> findAll() {
@@ -74,6 +65,6 @@ public class BogieRepository implements IBogieRepository {
 
     @Override
     public long count() {
-        return 0;
+        return bogieListing.size();
     }
 }
--- src/main/java/com/geektrust/example/geektrust/repositories/IBogieRepository.java
+++ src/main/java/com/geektrust/example/geektrust/repositories/IBogieRepository.java
@@ -4,6 +4,5 @@ import java.util.HashMap;
 import com.geektrust.example.geektrust.entities.Bogie;
 
 public interface IBogieRepository extends CRUDRepository<Bogie,String>{
-//    Bogie saveBoggy(Bogie bogie);
-    HashMap<String, Bogie> getBoggyMapping();
+    HashMap<String, Bogie> getBogieListing();
 }
